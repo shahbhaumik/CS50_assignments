a66407b21e38dcc9e614b9e386363e12221b8970
Bhaumik <shah.bhaumik@gmail.com>
2015-06-12 16:03:15 -0700

    Week 1 assignment_std version solution.

---
+++ CS50_assign1_std/main.c
@@ -0,0 +1,40 @@
+/*
+
+Created by Bhaumik Shah
+
+CS50 Week 1 assignment
+Used codeblocks with GCC compiler
+To make it C99 standard
+goto-> Project -> Build properties -> Compiler -> Other
+add "-std=c99"
+To include CS50 library
+Go under linker and add the lib manually
+
+*/
+
+
+#include <cs50.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{   int height;
+    do  {
+        printf("Enter an integer, whoes value is in between 0 to 9. \n");  //ask for the the height
+        height = GetInt();                                                 //get user specified height
+        printf("Value of a is %d\n", height);                              // printing the height
+        } while (height<0 || height>9);                                    // Checking if it is within the boundries
+
+    for (int row=1; row <= height; row++)                                  // Foop to create the pyramid, for loop iterates from 1 to height
+    {
+        for (int spaces=0; spaces <= height - row; spaces++)               // Number of spaces is equal to Height-Current row number
+            printf (" ");
+        for (int hashes =1; hashes <= row; hashes++)                       // Number of hash are equal to number of row
+            printf ("#");
+    printf("\n");                                                         // Next line
+    }
+    return 0;
+
+}
+
